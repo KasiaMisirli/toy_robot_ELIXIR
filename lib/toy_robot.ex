d1eb4bb63f0fbeaf1f9af39cefaacb8cf600b6a4
Kasia <kasia.misirli@gmail.com>
2019-06-11 16:54:50 +1000

    Add the left and right facing functions

--- lib/toy_robot.ex
+++ lib/toy_robot.ex
@@ -17,4 +17,42 @@ defmodule ToyRobot do
   def move(%{position: position}) do
     %{position: position + 1}
   end
+
+  @doc """
+  Turn left.
+
+  ## Examples
+
+      iex> ToyRobot.left(%{facing: "WEST"})
+      %{facing: "SOUTH"}
+
+    
+  """
+  def left(%{facing: facing}) do
+    case %{facing: facing} do
+      %{facing: "NORTH"} -> %{facing: "WEST"}
+      %{facing: "WEST"} -> %{facing: "SOUTH"}
+      %{facing: "EAST"} -> %{facing: "NORTH"}
+      %{facing: "SOUTH"} -> %{facing: "EAST"}
+    end
+  end
+
+  @doc """
+  Turn right.
+
+  ## Examples
+
+      iex> ToyRobot.right(%{facing: "WEST"})
+      %{facing: "NORTH"}
+
+    
+  """
+  def right(%{facing: facing}) do
+    case %{facing: facing} do
+      %{facing: "NORTH"} -> %{facing: "EAST"}
+      %{facing: "WEST"} -> %{facing: "NORTH"}
+      %{facing: "EAST"} -> %{facing: "SOUTH"}
+      %{facing: "SOUTH"} -> %{facing: "WEST"}
+    end
+  end
 end
--- test/toy_robot_test.exs
+++ test/toy_robot_test.exs
@@ -2,7 +2,15 @@ defmodule ToyRobotTest do
   use ExUnit.Case
   doctest ToyRobot
 
-  test "more complex stuff goes here" do
+  test "the robot moves" do
+    assert ToyRobot.move(%{position: 1}) == %{position: 2}
+  end
 
+  test "the robot turns towards left" do
+    assert ToyRobot.left(%{facing: "WEST"}) == %{facing: "SOUTH"}
+  end
+
+  test "the robot turns towards right" do
+    assert ToyRobot.right(%{facing: "WEST"}) == %{facing: "NORTH"}
   end
 end
